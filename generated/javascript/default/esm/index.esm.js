07fd598d110322c538e7886eb461b392b2eda6f8
SayedSadiq45 <sayedmohdsadiq.786@gmail.com>
2026-05-04 13:19:47 +0530

    Add files via upload

---
+++ generated/javascript/default/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'BlissCampIndia',
+  location: 'us-central1'
+};
+
